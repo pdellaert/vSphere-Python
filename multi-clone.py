ab8db379b35dc08e19fbe3970f5bdf6be9490a41
Philippe Dellaert <philippe@dellaert.org>
2013-12-16 21:19:14 +0100

    Update multi_clone to support folders, adding script to gather IPs of VMs

--- multi-clone.py
+++ multi-clone.py
@@ -23,6 +23,17 @@ def find_resource_pool(name):
 			return mor
 	return None
 
+def find_folder(name):
+	folders = con._get_managed_objects(MORTypes.Folder)
+	try:
+		for mor, folder_name in folders.iteritems():
+			print_verbose('Parsing folder %s' % folder_name)
+			if folder_name == name:
+				return mor
+	except IndexError:
+		return None
+	return None
+
 def run_post_script(name,ip):
 	print_verbose('Running post script: %s %s %s' % (post_script,name,ip))
 	retcode = subprocess.call([post_script,name,ip])
@@ -55,6 +66,7 @@ parser = argparse.ArgumentParser(description="Deploy a template into multiple VM
 parser.add_argument('-6', '--six', required=False, help='Get IPv6 address for VMs instead of IPv4', dest='ipv6', action='store_true')
 parser.add_argument('-b', '--basename', nargs=1, required=True, help='Basename of the newly deployed VMs', dest='basename', type=str)
 parser.add_argument('-c', '--count', nargs=1, required=False, help='Starting count, the name of the first VM deployed will be <basename>-<count>, the second will be <basename>-<count+1> (default=1)', dest='count', type=int, default=[1])
+parser.add_argument('-f', '--folder', nargs=1, required=False, help='The folder in which the new VMs should reside', dest='folder', type=str)
 parser.add_argument('-n', '--number', nargs=1, required=False, help='Amount of VMs to deploy (default=1)', dest='amount', type=int, default=[1])
 parser.add_argument('-p', '--post-script', nargs=1, required=False, help='Script to be called after each VM is created and booted. Arguments passed: name ip-address', dest='post_script', type=str)
 parser.add_argument('-r', '--resource-pool', nargs=1, required=False, help='The resource pool in which the new VMs should reside', dest='resource_pool', type=str)
@@ -70,6 +82,9 @@ ipv6		= args.ipv6
 amount 		= args.amount[0]
 basename 	= args.basename[0]
 count 		= args.count[0]
+folder 		= None
+if args.folder:
+	folder		= args.folder[0]
 post_script 	= None
 if args.post_script: 
 	post_script = args.post_script[0]
@@ -102,12 +117,24 @@ if template_vm is None:
 print_verbose('Template %s found' % template)
 
 # Verify the target Resource Pool exists
-print_verbose('Finding resource pool %s' % resource_pool)
-resource_pool_mor = find_resource_pool(resource_pool)
-if resource_pool_mor is None:
-	print 'ERROR: %s not found' % resource_pool
-	sys.exit(1)
-print_verbose('Resource pool %s found' % resource_pool)
+resource_pool_mor = None
+if resource_pool is not None:
+	print_verbose('Finding resource pool %s' % resource_pool)
+	resource_pool_mor = find_resource_pool(resource_pool)
+	if resource_pool_mor is None:
+		print 'ERROR: %s not found' % resource_pool
+		sys.exit(1)
+	print_verbose('Resource pool %s found' % resource_pool)
+
+# Verify the target folder exists
+folder_mor = None
+if folder is not None:
+	print_verbose('Finding folder %s' % folder)
+	folder_mor = find_folder(folder)
+	if folder_mor is None:
+		print 'ERROR: %s not found' % folder
+		sys.exit(1)
+	print_verbose('Folder %s found' % folder)
 
 # List with VM name elements for post script processing
 vms_to_ps = []
@@ -119,7 +146,7 @@ for a in range(1,amount+1):
 	if find_vm(vm_name):
 		print 'ERROR: %s already exists' % vm_name
 	else:
-		clone = template_vm.clone(vm_name, True, None, resource_pool_mor, None, None, False)
+		clone = template_vm.clone(vm_name, True, folder_mor, resource_pool_mor, None, None, False)
 		print_verbose('VM %s created' % vm_name)
 		
 		print_verbose('Booting VM %s' % vm_name)
